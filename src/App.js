7e751706769163b15b3b06a28f1d74bc73ca9b50
Nishchays-github <nishchayparashar2004@gmail.com>
2024-04-27 15:37:55 +0530

    cra

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ function App() {
       <header className="App-header">
         <h1 className='tracking-widest'>hello </h1>
         <h2 className='tracking-widest'>hello 2</h2>
-        <h2 className='tracking-widest'>hello 2</h2>
-        <h2 className='tracking-widest'>hello 2</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
