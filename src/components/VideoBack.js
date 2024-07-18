92e6e7f417529a20f0b5ff7f701e37d6d5e39974
Nishchay Parashar <135508771+Nishchays-github@users.noreply.github.com>
2024-07-18 17:22:16 +0530

    Update VideoBack.js

--- src/components/VideoBack.js
+++ src/components/VideoBack.js
@@ -14,7 +14,7 @@ const VideoBack = ({ id }) => {
         width="100%"
         height="800px"
         muted={true}
-        // playing={true} // Autoplay
+        playing={true}
         playbackRate={1}
       />
     </div>
